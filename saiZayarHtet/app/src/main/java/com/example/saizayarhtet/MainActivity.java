8320cfcc8a37fe6ffbf4e89eb954f0184e847b29
Sai Zayar Htet <53620449+SaiZayarHtet1999@users.noreply.github.com>
2019-11-19 11:46:15 +0630

    saiTextSize

--- saiZayarHtet/app/src/main/java/com/example/saizayarhtet/MainActivity.java
+++ saiZayarHtet/app/src/main/java/com/example/saizayarhtet/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.saizayarhtet;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int i=10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn=findViewById(R.id.sai);
+
+        final TextView saitext=findViewById(R.id.saiText);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i= 10;
+            }
+        });
+        saitext.setTextSize(i);
     }
+
+
 }
